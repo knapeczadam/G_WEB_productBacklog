7c3c9bdd844205dfbd4d5a9ac7235c7b824e7b8d
Papuss <mrpapsu@gmail.com>
2016-05-25 11:48:03 +0200

    css move to folder, UserName add on blacklog html,

--- Logout.js
+++ Logout.js
@@ -2,3 +2,7 @@ function deleteCookie() {
     document.cookie = 'logged=false';
     window.open('Login.html');
 }
+
+(function () {
+        document.getElementById("loggedName").innerHTML = localStorage.getItem("loggedIn");
+})();
