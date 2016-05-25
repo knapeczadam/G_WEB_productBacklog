22099f1ff84b5c78ccb9c9354045b2158cd2c135
Papuss <mrpapsu@gmail.com>
2016-05-25 14:01:40 +0200

    Redirect to new page

--- Login.js
+++ Login.js
@@ -16,7 +16,7 @@
 
     console.log(x);
     if (x == 'logged=true') {
-        window.open('backlog.html');
+        window.location.href = "backlog.html";
     }
 
 })();
@@ -27,15 +27,14 @@ function check(form) {
     var user1pass = localStorage.getItem("user1pass");
     var user2name = localStorage.getItem("user2name");
     var user2pass = localStorage.getItem("user2pass");
-    if (document.cookie != 'logged=true') {
         if (form.userName.value == user1name && form.password.value == user1pass) {
-            window.open('backlog.html');
+            window.location.href = "backlog.html";
             localStorage.setItem("loggedIn",user1name);
             if (form.rememberme.checked) {
                 document.cookie = 'logged=true';
             }
         } else if (form.userName.value == user2name && form.password.value == user2pass) {
-            window.open('backlog.html');
+            window.location.href = "backlog.html";
             localStorage.setItem("loggedIn",user2name);
             if (form.rememberme.checked) {
                 document.cookie = 'logged=true';
@@ -44,7 +43,6 @@ function check(form) {
         else {
             alert("Error password or Username");
         }
-    }
     console.log(document.cookie);
 }
 
